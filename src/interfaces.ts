dd4287ddd7f92c069bb43a52fe10929064ac418c
JasminFarhi <jasmin.farhi@dltx.io>
2022-07-20 09:28:02 +1000

    Removed tester

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -101,7 +101,7 @@ export interface IKycService {
 }
 
 export interface IUserService {
-    requestToBeTester(body: TestFlightRequest): Tester | PromiseLike<Tester>;
+    requestToBeTester(body: TestFlightRequest): PromiseLike<Tester>;
     verify(body: UserVerifyRequestBody): Promise<string>;
     findOne(email: string): Promise<User>;
     testflightTester(body: TestFlightRequest): Promise<Tester>;
