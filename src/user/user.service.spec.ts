e9d08d1b42efddedc59ffb64ed5635e335337a22
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2022-08-24 17:12:00 +1000

    Revert "setup test"
    This reverts commit 01e56f028122df6c0c61ad9ed62b2e53e7faf90d.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,50 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "./user.service";
 import { expect } from "chai";
-import { ConfigService } from "@nestjs/config";
-import { JwtService } from "@nestjs/jwt";
-import { getRepositoryToken } from "@nestjs/typeorm";
-import { User } from "../data/entities/user.entity";
-import { Tester } from "../data/entities/tester.entity";
-import { Request } from "../data/entities/request.entity";
+import { userProviders } from "./user.providers";
 
-const repositoryMockFactory = () => ({
-    findOne: entity => entity,
-    save: entity => entity,
-    update: entity => entity,
-    delete: entity => entity
-});
-
-describe("UserService", () => {
+//TODO: need to fix the unit test later
+xdescribe("UserService", () => {
     let service: UserService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
-                UserService,
-                ConfigService,
-                JwtService,
+                ...userProviders,
                 {
-                    provide: getRepositoryToken(User),
-                    useFactory: repositoryMockFactory
-                },
-                {
-                    provide: getRepositoryToken(Request),
-                    useFactory: repositoryMockFactory
-                },
-                {
-                    provide: getRepositoryToken(Tester),
-                    useFactory: repositoryMockFactory
-                },
-                {
-                    provide: "ISmsService",
-                    useFactory: () => ({})
-                },
-                {
-                    provide: "IEmailService",
-                    useFactory: () => ({
-                        sendEmailVerification: () => ({})
-                    })
+                    provide: "IUserService",
+                    useClass: UserService
                 }
             ]
         }).compile();
@@ -54,21 +23,5 @@ describe("UserService", () => {
 
     it("should be defined", () => {
         expect(service).to.be.not.undefined;
-    });
-
-    it("should sent email with token", async () => {
-        //spyOn(sendEmailVerification);
-        await service.addPublicKey({
-            publicKeyArmored: "test",
-            email: "email@test.com"
-        });
-        expect(false).to.be.true;
-    });
-
-    it("should veriy token", async () => {
-        const email = "email@test.com";
-        const token = "123"; // generate token from email
-        const result = await service.decodeEmailFromToken(token);
-        expect(result).to.equal(email);
     });
 });
