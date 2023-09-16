6615eaa8e1d90a0e98008ecacbd3b6dfebca7a34
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-09-16 13:23:06 +1000

    chore: remove api key strat

--- src/auth/apikey.strategy.ts
+++ src/auth/apikey.strategy.ts
@@ -1,28 +1,28 @@
-import { HeaderAPIKeyStrategy } from "passport-headerapikey";
-import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+// import { HeaderAPIKeyStrategy } from "passport-headerapikey";
+// import { PassportStrategy } from "@nestjs/passport";
+// import { Injectable, UnauthorizedException } from "@nestjs/common";
+// import { ConfigService } from "@nestjs/config";
 
-@Injectable()
-export class ApiKeyStrategy extends PassportStrategy(
-    HeaderAPIKeyStrategy,
-    "api-key"
-) {
-    constructor(
-        private configService: ConfigService
-    ) {
-        const headerKeyApiKey =
-            configService.get<string>("HEADER_KEY_API_KEY") || "";
+// @Injectable()
+// export class ApiKeyStrategy extends PassportStrategy(
+//     HeaderAPIKeyStrategy,
+//     "api-key"
+// ) {
+//     constructor(
+//         private configService: ConfigService
+//     ) {
+//         const headerKeyApiKey =
+//             configService.get<string>("HEADER_KEY_API_KEY") || "";
 
-        super(
-            { header: headerKeyApiKey, prefix: "" },
-            true,
-            async (apiKey, done) => {
-                // if (this.authService.validateApiKey(apiKey)) {
-                //     done(null, true);
-                // }
-                done(new UnauthorizedException(), null);
-            }
-        );
-    }
-}
+//         super(
+//             { header: headerKeyApiKey, prefix: "" },
+//             true,
+//             async (apiKey, done) => {
+//                 // if (this.authService.validateApiKey(apiKey)) {
+//                 //     done(null, true);
+//                 // }
+//                 done(new UnauthorizedException(), null);
+//             }
+//         );
+//     }
+// }
