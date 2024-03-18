e95e736ebc3cb80b667272f73c01299cea61e2a4
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-03-18 14:35:38 +1000

    feat: change axios

--- src/services/partners/GPIB.ts
+++ src/services/partners/GPIB.ts
@@ -1,6 +1,6 @@
 import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from "axios";
 import { IVendor, UserSignupRequest } from "../../interfaces";
 import { ConfigSettings } from "../../types/general";
 import moment from "moment";
@@ -11,14 +11,17 @@ export class GPIBVendor implements IVendor {
     private readonly logger = new Logger("GPIBVendor");
     private baseUrl: string;
 
-    constructor(private config: ConfigService, private axios: AxiosInstance) {
+    constructor(
+        private config: ConfigService,
+        private axiosInstance: AxiosInstance
+    ) {
         this.baseUrl = this.config.get(ConfigSettings.GPIB_API_ENDPOINT);
     }
 
     public async signUp(signupInfo: UserSignupRequest) {
         const { firstName, lastName, email, password, verification, mobile } =
             signupInfo;
-        const endPoint = `${this.baseUrl}/user/idem`;
+        const endPoint = `${this.baseUrl}/User/idem`;
         const referralCode = this.config.get(ConfigSettings.GPIB_REFERRAL_CODE);
         const requestBody = {
             firstName,
@@ -33,16 +36,27 @@ export class GPIBVendor implements IVendor {
             hashedPayload: "", //verification.hashedPayload,
             signature: "" // verification.signature
         };
-        const response = await this.axios
-            .post<SignupResponse>(endPoint, JSON.stringify(requestBody))
-            .catch(error => {
-                if (error.response) {
-                    this.logger.error(error.response.data);
-                    throw new Error(error.response.data);
-                }
-                this.logger.error(error.message);
-                throw error;
-            });
+
+        const response = await axios.request({
+            method: "POST",
+            url: "https://testapi.getpaidinbitcoin.com.au/User/idem",
+            data: requestBody,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        // const response = await this.axiosInstance
+        //     .post<SignupResponse>(endPoint, JSON.stringify(requestBody), config)
+        //     .catch(error => {
+        //         if (error.response) {
+        //             this.logger.error(error.response.data);
+        //             throw new Error(error.response.data);
+        //         }
+        //         this.logger.error(error.message);
+        //         throw error;
+        //     });
+
         const userId = response.data;
         return { userId };
     }
