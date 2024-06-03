f9b822ddb48f8cb2e1289743d1216a1d3e502bb3
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-06-04 08:03:04 +1000

    refactor: Update GPIB signup request keys to be capitalized

--- src/services/partners/GPIB.ts
+++ src/services/partners/GPIB.ts
@@ -4,8 +4,8 @@ import axios, { AxiosInstance } from "axios";
 import { IVendor, UserSignupRequest } from "../../interfaces";
 import { ConfigSettings } from "../../types/general";
 import moment from "moment";
+import crypto from "crypto";
 
-type SignupResponse = string;
 export class GPIBVendor implements IVendor {
     name = "GPIB";
     private readonly logger = new Logger("GPIBVendor");
@@ -21,20 +21,24 @@ export class GPIBVendor implements IVendor {
     public async signUp(signupInfo: UserSignupRequest) {
         const { firstName, lastName, email, password, verification, mobile } =
             signupInfo;
+
         const endPoint = `${this.baseUrl}/User/idem`;
         const referralCode = this.config.get(ConfigSettings.GPIB_REFERRAL_CODE);
+
+        // GPIB C# needs capitalised keys
         const requestBody = {
-            firstName,
-            lastName,
-            email,
-            password,
-            referralCode: referralCode ?? "",
-            mobile: mobile.trim(),
-            yob: moment(signupInfo.dob, "DD/MM/YYYY").year(),
-            trackAddress: true,
-            createAddress: true,
-            hashedPayload: "", //verification.hashedPayload,
-            signature: "" // verification.signature
+            FirstName: firstName,
+            LastName: lastName,
+            Email: email,
+            Password: password,
+            ReferralCode: referralCode ?? "",
+            Mobile: mobile.trim(),
+            YOB: moment(signupInfo.dob, "DD/MM/YYYY").year(),
+            TrackAddress: true,
+            CreateAddress: true,
+            HashedPayload: "", //verification.hashedPayload,
+            Signature: "", // verification.signature
+            Nonce: crypto.randomUUID()
         };
 
         const response = await axios.request({
